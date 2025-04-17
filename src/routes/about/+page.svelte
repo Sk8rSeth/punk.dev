8db91300c69b7fd3e5b9a3ff193a7a0b4784c128
Seth Howell <SethDoesDev@gmail.com>
2025-04-17 15:36:32 -0700

    about page mobile styles

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -15,16 +15,27 @@
 
 <style>
     .main-content-container {
-      height: 100%;
-      display: flex;
-      flex-direction: column;
-    }
-    .main-content-area {
-        grid-column: 1 / 25;
-        grid-row: 2 / 4;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
     }
 
     .content-area {
-      flex-grow: 1;
+    flex-grow: 1;
+    }
+
+    @media (min-width: 769px) {
+        .main-content-area {
+            grid-column: 1 / 25;
+            grid-row: 2 / 4;
+        }
+
+    }
+
+    @media (max-width: 768px) {
+        .main-content-area {
+            grid-column: 1;
+            grid-row: 2 / 6;
+        }
     }
 </style>
