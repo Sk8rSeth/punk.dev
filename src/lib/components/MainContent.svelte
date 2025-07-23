bafc11e76785d7eb98397975a751c9680e2b066a
Seth Howell <SethDoesDev@gmail.com>
2025-07-23 15:56:53 -0700

    updated .punk logo type

--- src/lib/components/MainContent.svelte
+++ src/lib/components/MainContent.svelte
@@ -4,7 +4,7 @@
             <h2 class="section-title">//Main</h2>
             <div class="content-area">
                 <p>Hello,</p>
-                <p>Right now this site is under construction. However, eventually it will be the hub for all dotPUNK content, products, logs and documentation, and links.</p>
+                <p>Right now this site is under construction. However, eventually it will be the hub for all <span class="punk">.punk</span> content, products, logs and documentation, and links.</p>
                 <p>thanks for visiting, and please sign up for the newsletter below for info about Pathlight.Maps and darlings.rip information.</p>
             </div>
         </div>
